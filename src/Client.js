9fc9f1986d0bf16ea89e8f48a58b4c3fb3ac81ce
brianli <brnli7@gmail.com>
2018-06-16 18:48:23 -0400

    Updated REST endpoint

--- src/Client.js
+++ src/Client.js
@@ -4,7 +4,7 @@ const Stomp = require('stompjs');
 
 var BASE_URL;
 if(process.env.NODE_ENV === 'production') {
-    BASE_URL = "http://18.191.44.25:3001/";
+    BASE_URL = "http://tactoe.bid:3001/";
 } else if(process.env.NODE_ENV === 'development') {
     BASE_URL = "http://localhost:3001/";
 }
