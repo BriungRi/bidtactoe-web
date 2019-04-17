908de517ba25e9ebbe3736abb822bf90f2e18dea
brianli <brnli7@gmail.com>
2019-04-17 13:53:52 -0400

    Updates

---
+++ precache-manifest.dc9d8d1e7f0911bd9f36b74e80d61d87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e91b1d9e9120dd4331d3",
+    "url": "/bidtactoe/static/js/runtime~main.165d53c4.js"
+  },
+  {
+    "revision": "0273ec72527c525ae164",
+    "url": "/bidtactoe/static/js/main.7df1d3a5.chunk.js"
+  },
+  {
+    "revision": "cabd0999410aaaf7f783",
+    "url": "/bidtactoe/static/js/2.dce11fec.chunk.js"
+  },
+  {
+    "revision": "0273ec72527c525ae164",
+    "url": "/bidtactoe/static/css/main.7481e391.chunk.css"
+  },
+  {
+    "revision": "b8832dc3fcc625275def7350613b864f",
+    "url": "/bidtactoe/index.html"
+  }
+];
